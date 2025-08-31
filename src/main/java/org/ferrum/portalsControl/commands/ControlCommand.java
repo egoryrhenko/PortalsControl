132d4fa3baa8c94c8f31934c2271e150a118d6d7
Egor <yrce@ukr.net>
2025-08-31 23:13:16 +0300

    fix

--- src/main/java/org/ferrum/portalsControl/commands/ControlCommand.java
+++ src/main/java/org/ferrum/portalsControl/commands/ControlCommand.java
@@ -10,71 +10,61 @@ import org.ferrum.portalsControl.listeners.PortalListener;
 import java.util.List;
 
 public class ControlCommand implements CommandExecutor, TabCompleter {
-    @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (strings.length == 2) {
-            switch (strings[0].toLowerCase()) {
-                case "world" -> {
-                    switch (strings[1].toLowerCase()) {
-                        case "off":
-                            PortalListener.blockWorld = true;
-                            ConfigManager.setValue("Block_teleport_to.world", true);
-                            commandSender.sendMessage(ConfigManager.getMessage("ToWorldPortalOff"));
-                            return true;
-                        case "on":
-                            PortalListener.blockWorld = false;
-                            ConfigManager.setValue("Block_teleport_to.world", false);
-                            commandSender.sendMessage(ConfigManager.getMessage("ToWorldPortalOn"));
-                            return true;
-                        default:
-                            commandSender.sendMessage(command.getUsage());
-                            return true;
-                    }
-                }
-                case "nether" -> {
-                    switch (strings[1].toLowerCase()) {
-                        case "off":
-                            PortalListener.blockNether = true;
-                            ConfigManager.setValue("Block_teleport_to.nether", true);
-                            commandSender.sendMessage(ConfigManager.getMessage("ToNetherPortalOff"));
-                            return true;
-                        case "on":
-                            PortalListener.blockNether = false;
-                            ConfigManager.setValue("Block_teleport_to.nether", false);
-                            commandSender.sendMessage(ConfigManager.getMessage("ToNetherPortalOn"));
-                            return true;
-                        default:
-                            commandSender.sendMessage(command.getUsage());
-                            return true;
-                    }
-                }
-                case "end" -> {
-                    switch (strings[1].toLowerCase()) {
-                        case "off":
-                            PortalListener.blockEnd = true;
-                            ConfigManager.setValue("Block_teleport_to.end", true);
-                            commandSender.sendMessage(ConfigManager.getMessage("ToEndPortalOff"));
-                            return true;
-                        case "on":
-                            PortalListener.blockEnd = false;
-                            ConfigManager.setValue("Block_teleport_to.end", false);
-                            commandSender.sendMessage(ConfigManager.getMessage("ToEndPortalOn"));
-                            return true;
-                        default:
-                            commandSender.sendMessage(command.getUsage());
-                            return true;
-                    }
-                }
-                default -> {
-                    commandSender.sendMessage(command.getUsage());
-                    return true;
-                }
-            }
-        } else if (strings[0].equals("reload") && commandSender.hasPermission("portalscontrol.reload")) {
-            ConfigManager.loadConfig();
-            commandSender.sendMessage("PortalsControl config reloaded");
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage(command.getUsage());
+            return true;
         }
-        commandSender.sendMessage(command.getUsage());
+
+        if (args[0].equalsIgnoreCase("reload") && sender.hasPermission("portalscontrol.reload")) {
+            ConfigManager.loadConfig();
+            sender.sendMessage("§aPortalsControl config reloaded.");
+            return true;
+        }
+
+        String worldType = args[0].toLowerCase();
+        Boolean isBlock = null;
+
+        if (args.length == 2) {
+            if (args[1].equalsIgnoreCase("on")) isBlock = false;
+            if (args[1].equalsIgnoreCase("off")) isBlock = true;
+        }
+
+        switch (worldType) {
+            case "world" -> {
+                if (isBlock != null) {
+                    PortalListener.blockWorld = isBlock;
+                    ConfigManager.setValue("Block_teleport_to.world", isBlock);
+                    sender.sendMessage(ConfigManager.getMessage(isBlock ? "ToWorldPortalOff" : "ToWorldPortalOn"));
+                } else {
+                    sender.sendMessage(ConfigManager.getMessage(PortalListener.blockWorld ? "ToWorldPortalOff" : "ToWorldPortalOn"));
+                }
+                return true;
+            }
+            case "nether" -> {
+                if (isBlock != null) {
+                    PortalListener.blockNether = isBlock;
+                    ConfigManager.setValue("Block_teleport_to.nether", isBlock);
+                    sender.sendMessage(ConfigManager.getMessage(isBlock ? "ToNetherPortalOff" : "ToNetherPortalOn"));
+                } else {
+                    sender.sendMessage(ConfigManager.getMessage(PortalListener.blockNether ? "ToNetherPortalOff" : "ToNetherPortalOn"));
+                }
+                return true;
+            }
+            case "end" -> {
+                if (isBlock != null) {
+                    PortalListener.blockEnd = isBlock;
+                    ConfigManager.setValue("Block_teleport_to.end", isBlock);
+                    sender.sendMessage(ConfigManager.getMessage(isBlock ? "ToEndPortalOff" : "ToEndPortalOn"));
+                } else {
+                    sender.sendMessage(ConfigManager.getMessage(PortalListener.blockEnd ? "ToEndPortalOff" : "ToEndPortalOn"));
+                }
+                return true;
+            }
+            default -> sender.sendMessage(ConfigManager.getMessage("UnknownWorldType") + worldType);
+        }
+
+        sender.sendMessage(command.getUsage());
         return true;
     }
 
